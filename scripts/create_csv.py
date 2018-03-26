e1e14fae1be0a26bab72b948d08249593a809694
BzhTobal <alex.brault35@gmail.com>
2018-03-26 14:39:58 +0200

    ajout de la fonction dans processing

--- scripts/create_csv.py
+++ scripts/create_csv.py
@@ -8,52 +8,52 @@ Created on Tue Dec  5 15:44:20 2017
 
 from tellus_tools.survey_reader import survey_reader
 from tellus_tools.radar_tools import *
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import os 
 import csv
 
 
-filename = "DAT_0001.sgy"
+def createtocsv(filename):
 
 # create a seg-y manipulator
-seg = survey_reader(filename)
+    seg = survey_reader(filename)
 
 # create a group of traces from seg-y
-rad_img = radargram(seg.get_traces())
+    rad_img = radargram(seg.get_traces())
 
 # extracte some traces
-rad_sample  = rad_img.read_trace([0,-1,10]) 
-gps_sample  = rad_img.read_position_meter([0,-1,10])
+    rad_sample  = rad_img.read_trace([0,-1,10]) 
+    gps_sample  = rad_img.read_position_meter([0,-1,10])
 
 
 
-test = rad_sample.T
+    test = rad_sample.T
 
-valeurs = [""]*len(test)
+    valeurs = [""]*len(test)
 
-for i in range(len(test)):
-    print(i)
-    valeurs[i]= [i,gps_sample[1][i], gps_sample[0][i], gps_sample[2][i], test[i]]
-
-
+    for i in range(len(test)):
+        print(i)
+        valeurs[i]= [i,gps_sample[1][i], gps_sample[0][i], gps_sample[2][i], test[i]]
 
 
 
-entetes = [
-    u'Trace',
-     u'X',
-     u'Y',
-     u'Z',
-     u'Valeurs',
-]
 
 
-f = open('DAT_0001.csv', 'w')
-ligneEntete = ";".join(entetes) + "\n"
-writer = csv.writer(f, delimiter=";")
-f.write(ligneEntete)    
-writer.writerows(valeurs)
+    entetes = [
+        u'Trace',
+        u'X',
+        u'Y',
+        u'Z',
+        u'Valeurs',
+        ]
 
-f.close()
+
+    f = open('DAT_0001.csv', 'w')
+    ligneEntete = ";".join(entetes) + "\n"
+    writer = csv.writer(f, delimiter=";")
+    f.write(ligneEntete)    
+    writer.writerows(valeurs)
+
+    f.close()
 
 
